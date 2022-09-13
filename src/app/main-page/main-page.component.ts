a4984908580b24869432adbca28e3853c4379449
Paula Andrea Arguello Gutierrez <68118520+paularguello07@users.noreply.github.com>
2022-09-13 09:34:03 -0500

    soy

--- src/app/main-page/main-page.component.ts
+++ src/app/main-page/main-page.component.ts
@@ -22,7 +22,7 @@ export class MainPageComponent implements OnInit {
 
   ngOnInit() {
 
-    /*this.authService.getUserLogged().then((res)=>{
+    this.authService.getUserLogged().then((res)=>{
     if(!res){
       this.router.navigate(['/login'])
     }else{
@@ -35,11 +35,111 @@ export class MainPageComponent implements OnInit {
 
     
     
-  })*/
-    
+  })
+
   }
   logout() {
     this.authService.logout();
-    this.router.navigate(['/login'])
+    this.router.navigate(['/landing-page'])
   }
+
+  //Para las funciones del calendario y del administrador de tareas
+
+//Calendario:
+/*let monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril','Mayo','Junio','Julio','Agosto', 'Septiembre','Octubre','Noviembre', 'Diciembre'];
+
+let currentDate = new Date();
+let currentDay = currentDate.getDate();
+let monthNumber = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
+
+let dates = document.getElementById('dates');
+let month = document.getElementById('month');
+let year = document.getElementById('year');
+
+let prevMonthDOM = document.getElementById('prev-month');
+let nextMonthDOM = document.getElementById('next-month');
+// 0 enero, 11 diciembre
+
+
+month.textContent = monthNames[monthNumber];
+year.textContent = currentYear.toString();
+
+prevMonthDOM.addEventListener('click', ()=>lastMonth());
+nextMonthDOM.addEventListener('click', ()=>nextMonth());
+
+
+
+const writeMonth = (month) => {
+
+    for(let i = startDay(); i>0;i--){
+        dates.innerHTML += ` <div class="Cal_date Cal_item Cal_last-days">
+            ${getTotalDays(monthNumber-1)-(i-1)}
+        </div>`;
+    }
+
+    for(let i=1; i<=getTotalDays(month); i++){
+        if(i===currentDay) {
+            dates.innerHTML += ` <div class="Cal_date Cal_item Cal_today">${i}</div>`;
+        }else{
+            dates.innerHTML += ` <div class="Cal_date Cal_item">${i}</div>`;
+        }
+    }
+}
+
+const getTotalDays = month => {
+    if(month === -1) month = 11;
+
+    if (month == 0 || month == 2 || month == 4 || month == 6 || month == 7 || month == 9 || month == 11) {
+        return  31; //meses con 31 días
+
+    } else if (month == 3 || month == 5 || month == 8 || month == 10) {
+        return 30; //meses con 30 días
+
+    } else {
+
+        return isLeap() ? 29:28; //Para el año biciesto true -> 29, false -> 28
+    }
+}
+
+//festivos
+const isLeap = () => {
+    return ((currentYear % 100 !==0) && (currentYear % 4 === 0) || (currentYear % 400 === 0));
+}
+
+const startDay = () => {
+    let start = new Date(currentYear, monthNumber, 1);
+    return ((start.getDay()-1) === -1) ? 6 : start.getDay()-1; //empiece en lunes
+}
+
+const lastMonth = () => {
+    if(monthNumber !== 0){
+        monthNumber--;
+    }else{
+        monthNumber = 11;
+        currentYear--;
+    }
+
+    setNewDate();
+}
+
+const nextMonth = () => {
+    if(monthNumber !== 11){
+        monthNumber++;
+    }else{
+        monthNumber = 0;
+        currentYear++;
+    }
+
+    setNewDate();
+}
+
+const setNewDate = () => {
+    currentDate.setFullYear(currentYear,monthNumber,currentDay);
+    month.textContent = monthNames[monthNumber];
+    year.textContent = currentYear.toString();
+    dates.textContent = '';
+    writeMonth(monthNumber);
+}*/
+
 }
