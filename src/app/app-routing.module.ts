f9532ab315bee1336441c98f0b9138c691bd443c
Paula Andrea Arguello Gutierrez <68118520+paularguello07@users.noreply.github.com>
2022-09-14 22:07:58 -0500

    sin config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './Auth/login/login.component';
-import { RegisterComponent } from './Auth/register/register.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
-import { ConfigComponent } from './Auth/config/config.component';
 
 const routes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  },
   {
     path:'',
     redirectTo:'landing-page', 
     pathMatch: 'full' 
   },
-  {
-    path: 'register',
-    component: RegisterComponent
-  },
+ 
   {
     path: 'main-page',
     component: MainPageComponent
@@ -28,10 +18,7 @@ const routes: Routes = [
     path: 'landing-page',
     component: LandingPageComponent
   },
-  {
-    path: 'config',
-    component: ConfigComponent
-  }
+  
 
 
 ];
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-import { LoginComponent } from './Auth/login/login.component';
 import { environment } from 'src/environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
-import { RegisterComponent } from './Auth/register/register.component';
 import { FormsModule } from '@angular/forms';
 import { MainPageComponent } from './main-page/main-page.component';
-import { ConfigComponent } from './Auth/config/config.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 
 
@@ -18,10 +15,7 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegisterComponent,
     MainPageComponent,
-    ConfigComponent,
     LandingPageComponent
   ],
   imports: [
